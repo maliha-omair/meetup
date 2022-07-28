40b9ca04adbf20e94f802cfc77b715b2db89befa
Maliha Omair <maliha@malihaomair.com>
2022-07-28 16:24:25 -0400

    modified app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+const { ValidationError } = require('sequelize');
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -15,21 +17,25 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+  // backend/app.js
+const routes = require('./routes');
+app.use(routes); // Connect all the routes
+
 // Security Middleware
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
-  }
+}
   
   // helmet helps set a variety of headers to better secure your app
-  app.use(
+app.use(
     helmet.crossOriginResourcePolicy({ 
       policy: "cross-origin" 
     })
-  );
+);
   
   // Set the _csrf token and create req.csrfToken method
-  app.use(
+app.use(
     csurf({
       cookie: {
         secure: isProduction,
@@ -37,11 +43,42 @@ if (!isProduction) {
         httpOnly: true
       }
     })
-  );
-  // backend/app.js
-  const routes = require('./routes');
-  app.use(routes); // Connect all the routes
+);
+
+
+  // Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    console.log("error because of formatter ",err)
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+   
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+  // Process sequelize errors
+app.use((err, _req, _res, next) => {
+        // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+
+
 
   module.exports = app;
-
-  
