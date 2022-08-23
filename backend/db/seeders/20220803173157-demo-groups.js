f2e034a66118fb73790d977a19838f30fdb911a8
Maliha Omair <maliha@malihaomair.com>
2022-08-23 18:01:10 -0400

    updated seed file added demo user, groups and events

--- backend/db/seeders/20220803173157-demo-groups.js
+++ backend/db/seeders/20220803173157-demo-groups.js
@@ -3,21 +3,21 @@ const { faker } = require('@faker-js/faker');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    let groups = [];
-    for(let i=0; i<5; i++){
-      groups.push({
-        name: faker.random.words(3),
-        organizerId: 5,
-        about: faker.lorem.words(70),
-        type: i%2===0?"In person":"Online",
-        private: true,
-        city: faker.address.cityName(),
-        state: faker.address.state()
-      })
-    }
-    await queryInterface.bulkInsert('Groups',groups, {});
+    // let groups = [];
+    // for(let i=0; i<5; i++){
+    //   groups.push({
+    //     name: faker.random.words(3),
+    //     organizerId: 5,
+    //     about: faker.lorem.words(70),
+    //     type: i%2===0?"In person":"Online",
+    //     private: true,
+    //     city: faker.address.cityName(),
+    //     state: faker.address.state()
+    //   })
+    // }
+    // await queryInterface.bulkInsert('Groups',groups, {});
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Groups', null, {});   
+    // await queryInterface.bulkDelete('Groups', null, {});   
 }
 };
