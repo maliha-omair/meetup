fb3c9cf0759409336fcb789c68d120707ff1f3f3
Maliha Omair <maliha@malihaomair.com>
2022-08-24 12:24:46 -0400

    added about Meetup page

--- frontend/src/component/AboutMeetup/index.js
+++ frontend/src/component/AboutMeetup/index.js
@@ -1,9 +1,21 @@
 import styles from "../AboutMeetup/AboutMeetup.module.css"
+import image from "../../assets/aboutMeetup.jpg"
+import { useHistory } from 'react-router-dom';
 export default function AboutMeetup(){
+    const history = useHistory()
     return(
         <div className={styles.main}>
-            <div>
-
+           <div className={styles.subDiv}>
+                <div className={styles.heading}>
+                    Meetup creates possibilities
+                </div>
+                <div className={styles.paragraph}>
+                    Meetup is a platform for finding and building local communities. People use Meetup to meet new people, learn new things, find support, get out of their comfort zones, and pursue their passions, together.
+                </div>
+                <button className={styles.button} onClick={()=>history.push("/login")}> Join Meetup</button>
+            </div>
+            <div style={{ backgroundImage: `url(${image})` }}>
+                <img src={image}></img>
             </div>
         </div>
     )
